11589f8bb36462059027a8e38b61bc008249f510
Cristian David Corrales Ospina <ccorrales@PASCUALBRAVO.CENTRAL>
2023-04-27 13:13:23 -0500

    Hecho el renderizado del canvas

--- game.js
+++ game.js
@@ -1,13 +1,32 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
-window.addEventListener('load',startGame);
+let canvasSize;
+let elementsSize;
+
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
 function startGame() {
-    game.fillRect(0,50,100,100);
-    game.clearRect(0,50,50,50);
-    game.font="25px Verdana";
-    game.fillStyle="purple";
-    game.textAlign="start";
-    game.fillText("Death",25,25);
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "start";
+
+  for (let i = 0; i < 10; i++) {
+    game.fillText(emojis["X"], elementsSize * i, elementsSize);
+  }
+}
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+
+  elementsSize = canvasSize / 10 - 1;
+
+  startGame();
 }
